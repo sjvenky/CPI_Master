0d9aa4e2dab96cdc282412deceb45f688065f209
sap integration suite <110297875+sjvenky@users.noreply.github.com>
2022-08-04 13:08:46 +0530

    Smoke Test Scenario
    Build sample scenario for testing interface

---
+++ Smoke Test Scenario/src/main/resources/script/script1.groovy
@@ -0,0 +1,15 @@
+
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+def Message processData(Message message) {
+    def body = message.getBody(java.lang.String) as String;
+    def messageLog = messageLogFactory.getMessageLog(message);
+    if(messageLog != null){
+        messageLog.setStringProperty("Logging#1", "Printing Payload As Attachment")
+        messageLog.addAttachmentAsString("ResponsePayload:", body, "text/plain");
+     }
+    return message
+}
+
+
+
